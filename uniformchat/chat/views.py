be26ed451772db64ca6dea0326060c894b4904ef
phammichael1001 <13736+phammichael1001@users.noreply.github.tamu.edu>
2022-04-06 11:08:55 -0500

    changed test db

--- uniformchat/chat/views.py
+++ uniformchat/chat/views.py
@@ -119,8 +119,8 @@ def groupMe_auth(request: HttpRequest):
     text_file.write("groupme:" + request.GET.get('access_token') + "\n")
     text_file.close()
 
-    groupme_db = "INSERT INTO groupme (groupme_user, groupme_autho) VALUES ( %s, %s)"
-    groupme_dbvalue = ('test',request.GET.get('access_token'))
+    groupme_db = "INSERT INTO groupme (groupme_user, groupme_autho) VALUES ( %s, %s, %s, %s, %s)"
+    groupme_dbvalue = ('test','test','0','request.GET.get('access_token')','0')
 
     cur.execute(groupme_db, groupme_dbvalue)
     conn.commit()
